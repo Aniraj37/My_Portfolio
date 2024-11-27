2d2043b988d3cb023fb96ef083a20b06ce3740fd
aniraj37 <aniraj.khadgi@apexcollege.edu.np>
2024-11-27 23:37:57 +1100

    Layout_Start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Feedbacks from "./components/Feedbacks";
+import Hero from "./components/Hero";
+import Loader from "./components/Loader";
+import Navbar from "./components/Navbar";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+
+const App = () => {
+  return <div>3D Developer Portfolio</div>;
+};
+
+export default App;
