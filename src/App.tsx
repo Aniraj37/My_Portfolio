ad480095b9e930d3ab02c5541617414ab44105f8
Aniraj37 <aniraj.khadgi@apexcollege.edu.np>
2024-11-28 00:46:20 +1100

    Layout_End

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,28 @@ import Loader from "./components/Loader";
 import Navbar from "./components/Navbar";
 import Tech from "./components/Tech";
 import Works from "./components/Works";
+import { StarsCanvas } from "./components/canvas";
 
 const App = () => {
-  return <div>3D Developer Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
